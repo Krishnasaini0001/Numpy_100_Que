80014d708d99f1443c207fd6e8c82a6056862afa
Krishnasaini0001 <krishna.saini2023@sait.ac.in>
2026-09-08 13:08:15 +0530

    Convert Integer Array to Float

---
+++ 020_problem.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+arr = np.array([10, 20, 30, 40])
+
+float_arr = arr.astype(float)
+
+print("Original array:", arr)
+print("Converted array:", float_arr)
+print("Data type:", float_arr.dtype)
