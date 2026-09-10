e05a2ad8fd25c2d8539b9da1405ee444c494d375
Krishnasaini0001 <krishna.saini2023@sait.ac.in>
2026-09-10 22:32:53 +0530

    2D Array Indexing

---
+++ 029_problem.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+arr = np.array([
+    [10, 20, 30],
+    [40, 50, 60],
+    [70, 80, 90]
+])
+
+print("First row:", arr[0])
+print("Element at row 2, column 3:", arr[1, 2])
+print("Last element:", arr[-1, -1])
