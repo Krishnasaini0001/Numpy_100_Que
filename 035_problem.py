4f88c9fffd4f97013a00f53ed4f88847af1062e6
Krishnasaini0001 <krishna.saini2023@sait.ac.in>
2026-09-11 15:36:18 +0530

    Concatenate Two Arrays

---
+++ 035_problem.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+a = np.array([1, 2, 3])
+b = np.array([4, 5, 6])
+
+result = np.concatenate((a, b))
+
+print("Array A:", a)
+print("Array B:", b)
+print("Concatenated:", result)
