eb93a0c4b1085db39289d7449c3fad516ec96269
Krishnasaini0001 <krishna.saini2023@sait.ac.in>
2026-09-11 15:26:05 +0530

    reshaping an array

---
+++ 027_problem.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+arr = np.array([10, 20, 30, 40, 50, 60])
+
+print("First three:", arr[:3])
+print("Middle elements:", arr[2:5])
+print("Last three:", arr[-3:])
---
+++ 031_problem.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+arr = np.arange(1, 13)
+
+reshaped = arr.reshape(3, 4)
+
+print("Original:")
+print(arr)
+
+print("\nReshaped:")
+print(reshaped)
